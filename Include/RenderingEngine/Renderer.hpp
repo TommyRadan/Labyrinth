be84fe1ad2217dd496e670dcfaa3cda7e60cd07f
Tomislav Radanović <tommy.radan@gmail.com>
2017-11-04 03:23:52 +0100

    Added Renderer

---
+++ Include/RenderingEngine/Renderer.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <glm.hpp>
+#include <string>
+
+#include <Infrastructure/Component.hpp>
+
+namespace RenderingEngine
+{
+    struct Renderer : public Component
+    {
+        void StartRenderer();
+        void StopRenderer();
+
+        virtual void SetupCamera() = 0;
+
+        void UploadTextureReference(const std::string &textureName, int position);
+        void UploadCoefficient(const std::string &coefficientName, float coefficient);
+        void UploadMatrix3(const std::string &mat3Name, const glm::mat3 &matrix);
+        void UploadMatrix4(const std::string &mat4Name, const glm::mat4 &matrix);
+        void UploadVector2(const std::string &vec2Name, const glm::vec2 &vector);
+        void UploadVector3(const std::string &vec3Name, const glm::vec3 &vector);
+        void UploadVector4(const std::string &vec4Name, const glm::vec4 &vector);
+
+    protected:
+        Renderer() = default;
+
+        void ConstructProgram(const std::string& vsString, const std::string& fsString);
+        void DestructProgram();
+
+        void *m_VertexShader;
+        void *m_FragmentShader;
+        void *m_Program;
+    };
+}
---
+++ Source/RenderingEngine/Renderer.cpp
@@ -0,0 +1,89 @@
+#include <RenderingEngine/Renderer.hpp>
+#include <RenderingEngine/OpenGL/OpenGL.hpp>
+
+void RenderingEngine::Renderer::StartRenderer()
+{
+    static_cast<OpenGL::Program*>(m_Program)->Start();
+}
+
+void RenderingEngine::Renderer::StopRenderer()
+{
+    static_cast<OpenGL::Program*>(m_Program)->Stop();
+}
+
+void RenderingEngine::Renderer::UploadTextureReference(const std::string& textureName, const int position)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(textureName);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, position);
+}
+
+void RenderingEngine::Renderer::UploadCoefficient(const std::string& coefficientName, const float coefficient)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(coefficientName);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, coefficient);
+}
+
+void RenderingEngine::Renderer::UploadMatrix3(const std::string& mat3Name, const glm::mat3& matrix)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(mat3Name);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, matrix);
+}
+
+void RenderingEngine::Renderer::UploadMatrix4(const std::string& mat4Name, const glm::mat4& matrix)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(mat4Name);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, matrix);
+}
+
+void RenderingEngine::Renderer::UploadVector2(const std::string& vec2Name, const glm::vec2& vector)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(vec2Name);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, vector);
+}
+
+void RenderingEngine::Renderer::UploadVector3(const std::string& vec3Name, const glm::vec3& vector)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(vec3Name);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, vector);
+}
+
+void RenderingEngine::Renderer::UploadVector4(const std::string& vec4Name, const glm::vec4& vector)
+{
+    OpenGL::Uniform uniform = static_cast<OpenGL::Program*>(m_Program)->GetUniform(vec4Name);
+    if (uniform == -1) return;
+    static_cast<OpenGL::Program*>(m_Program)->SetUniform(uniform, vector);
+}
+
+void RenderingEngine::Renderer::ConstructProgram(const std::string& vsString, const std::string& fsString)
+{
+    m_VertexShader = new OpenGL::Shader(OpenGL::ShaderType::Vertex);
+    m_FragmentShader = new OpenGL::Shader(OpenGL::ShaderType::Fragment);
+    m_Program = new OpenGL::Program();
+
+    auto vs = (OpenGL::Shader*) m_VertexShader;
+    auto fs = (OpenGL::Shader*) m_FragmentShader;
+    auto prog = (OpenGL::Program*) m_Program;
+
+    vs->Source(vsString);
+    vs->Compile();
+
+    fs->Source(fsString);
+    fs->Compile();
+
+    prog->Attach(*vs);
+    prog->Attach(*fs);
+    prog->Link();
+}
+
+void RenderingEngine::Renderer::DestructProgram()
+{
+    delete static_cast<OpenGL::Shader*>(m_VertexShader);
+    delete static_cast<OpenGL::Shader*>(m_FragmentShader);
+    delete static_cast<OpenGL::Program*>(m_Program);
+}
