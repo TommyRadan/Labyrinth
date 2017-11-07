b66539dc26e73021bf30c800cf113f7196bbdfb0
Tomislav Radanović <tommy.radan@gmail.com>
2017-11-07 22:11:10 +0100

    Expanded Model to send UV's and Normals to the GPU

--- Source/RenderingEngine/Model.cpp
+++ Source/RenderingEngine/Model.cpp
@@ -16,6 +16,14 @@ void RenderingEngine::Model::UploadMesh(const RenderingEngine::Mesh& mesh)
     m_VertexArrayObject.BindAttribute(0, m_VertexBufferObject,
                                       RenderingEngine::OpenGL::Type::Float,
                                       3, sizeof(RenderingEngine::Vertex), 0);
+
+    m_VertexArrayObject.BindAttribute(1, m_VertexBufferObject,
+                                      RenderingEngine::OpenGL::Type::Float,
+                                      2, sizeof(RenderingEngine::Vertex), sizeof(glm::vec3));
+
+    m_VertexArrayObject.BindAttribute(2, m_VertexBufferObject,
+                                      RenderingEngine::OpenGL::Type::Float,
+                                      3, sizeof(RenderingEngine::Vertex), sizeof(glm::vec3) + sizeof(glm::vec2));
 }
 
 void RenderingEngine::Model::Render()
@@ -24,6 +32,5 @@ void RenderingEngine::Model::Render()
 
     RenderingEngine::OpenGL::Context::GetInstance()->DrawArrays(m_VertexArrayObject,
                                                                 RenderingEngine::OpenGL::Primitive::Triangles,
-                                                                0,
-                                                                m_VertexCount);
+                                                                0, m_VertexCount);
 }
