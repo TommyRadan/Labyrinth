39246cd443df809f6cb9d4141f0de4261e58f71a
Tino Melvan <tmelva00@fesb.hr>
2017-12-24 11:44:57 +0100

    Removing unecessary const's

--- Include/Utility/Random.hpp
+++ Include/Utility/Random.hpp
@@ -9,22 +9,22 @@ class Random
 {
  public:
     Random();
-    explicit Random(const uint32_t seed);
+    explicit Random(uint32_t seed);
 
     int32_t Next() const;
-    int32_t Next(const int32_t ceiling) const;
-    int32_t Next(const int32_t floor, const int32_t ceiling) const;
+    int32_t Next(int32_t ceiling) const;
+    int32_t Next(int32_t floor, int32_t ceiling) const;
 
     double NextDouble() const;
     float NextFloat() const;
 
     template <typename T, size_t Size>
-    void ShuffleArray(T(&arr)[Size]);
+    void ShuffleArray(T(&array)[Size]);
 
  private:
     std::default_random_engine& globalURNG() const;
     void Randomize() const;
-    void Randomize(const uint32_t seed) const;
+    void Randomize(uint32_t seed) const;
 };
 
 template <typename T, size_t Size>
